49da9746e916e506e474427f5cbf3960914438ff
Chandra Sekhar Guntupalli <chandug1505@gmail.com>
2014-12-05 23:29:04 +0530

    DAA MID-SEM-2 Solutions

--- DAA/B120618CS_2_20_Guntupalli Chandra Sekhar/Code_2_20.cpp
+++ DAA/B120618CS_2_20_Guntupalli Chandra Sekhar/Code_2_20.cpp
@@ -7,10 +7,15 @@ static const int MOD      = 1000000007;
 static const int MAX_BITS = 11;
 static const int MAX_N    = 2000; 
 long dp[MAX_N + 1][1 << MAX_BITS][2];
+
+class WinterAndSnowmen{
+	int recursiveSolution(int,int,int);
+	public:
+		int getNumber(int,int);
+		int i, N, M;
+};
  
-int i, N, M;
- 
-int rec(int t, int z, int b,int i)
+int WinterAndSnowmen::recursiveSolution(int t, int z, int b)
 {
     long res = dp[t][z][b];
     if (res == -1) {
@@ -23,14 +28,14 @@ int rec(int t, int z, int b,int i)
             }
         } else {
             // xor number t to neither A or B:
-            res = rec(t-1, z, b,i);
+            res = recursiveSolution(t-1, z, b);
             // xor number t to A:
             if (t <= N) {
-                res += rec(t-1, z ^ (t >> i), b ^ ( (t >> i) & 1 ),i );
+                res += recursiveSolution(t-1, z ^ (t >> i), b ^ ( (t >> i) & 1 ) );
             }
             // xor number q = p+1 to B:
             if (t <= M) {
-                res += rec(t-1, z ^ (t >> i), b,i);
+                res += recursiveSolution(t-1, z ^ (t >> i), b);
             }
         }
         res %= MOD;
@@ -40,19 +45,13 @@ int rec(int t, int z, int b,int i)
     return (int)res;
 }
  
-int getNumber(int n, int m)
+int WinterAndSnowmen::getNumber(int N, int M)
 {
     long res = 0;
     for (int i = 0; i < MAX_BITS; i++) {
-        // first difference will be in i-th bit
-        //                             i
-        //                  B  = abcdef1??????
-        //                  A  = abcdef0??????
-        //              A ^ B  = 0000001??????
-        // (A >> i) ^ (B >> i) = 0000001
         memset(dp, -1, sizeof(dp));
-        N=n; M=m; 
-        res += rec( max(N,M), 0, 0,i);
+        this->N=N; this->M=M; this->i =i; 
+        res += recursiveSolution( max(N,M), 0, 0);
     }
     return (int)(res % MOD);
 }
@@ -61,7 +60,8 @@ int main(){
 	int n,m,val;
 	cin>>n;
 	cin>>m;
-	val = getNumber(n,m);
+	WinterAndSnowmen was;
+	val = was.getNumber(n,m);
 	cout<<"Returns: "<<val<<endl;
 	return 0;
 }
